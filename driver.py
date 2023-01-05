b0a2dc5e8d7e223c95277c59457209ecb78c720f
Sid <siddharth.vivek2000@outlook.com>
2023-01-05 12:10:08 +0530

    Bunch of commits. In a nutshell, added the login system to the main file, did some cleanup (more cleanup needed), code should run pretty well on Python 3.10.5 now (#7)
    * Major cleanup & refactoring
    
    * Major cleanup & refactoring
    
    * Make user prompts prettier
    
    * Minor cleanup
    
    * Fix issue in title printing
    Fix the issue where terminals with smaller widths would wrap text of ascii_title
    causing title to become illegible
    
    * Write logic for signup and login
    - Can hash passwords with SHA-256
    - Stores user records in CSV file LOCALLY
    - Will implement ability to save solved boards LOCALLY in the future
    
    * Add ability to create USERDATA folder for new users
    - This will be the folder where saved boards will be stored
    
    * Remove venv folder
    
    * Remove __pycache__ binaries from git repo
    
    * Implement login system in driver file, make small modifications & cleanup

--- driver.py
+++ driver.py
@@ -1,11 +1,12 @@
 import sys
-from os import getcwd
-from os import name as os_name
-from os import system
+import shutil
+import os
+import re
 
 import board_prettyprint
 import board_solver
 import console_user_input
+import user_account_control as uac
 
 try:
     import inquirer
@@ -13,17 +14,90 @@ except ModuleNotFoundError:
     exc_type, exc_value, exc_trace = sys.exc_info()
     missing_module = getattr(exc_value, 'name')
 
-    errtext = f"A required 3rd party module: '{missing_module}' is not installed!\nTo automatically install all requirements, type the following command into your terminal:\npip install -r {getcwd()}\\requirements.txt\n\nOr, activate the virtual environment using:\n{getcwd()}\\venv\\Scripts\\Activate"
+    errtext = f"A required 3rd party module: '{missing_module}' is not installed!\n\
+To automatically install all requirements, type the following command into your terminal:\n\
+pip install -r {os.getcwd()}\\requirements.txt\n\n\
+Or, activate the virtual environment using:\n{os.getcwd()}\\venv\\Scripts\\Activate"
+
     print(errtext)
     sys.exit(1)
-
 
 title_file = "assets/ascii_title.txt"
 help_file = "assets/ascii_instructions.txt"
 
 
+def validate_login(uname, pwd):
+    if not re.search('(?=.*[a-zA-Z0-9])(?=.*[-+_!@#$%^&*.,?])', pwd): # Reegex for checking password validity
+        return False, 'Password must contain:\n\
+- At least one upper-case character\n\
+- At least one lower-case character\n\
+- At least one number\n\
+- At least one special character'
+    
+    if len(pwd) < 8:
+        return False, 'Password must be at least 8 characters long!'
+
+    if uname == pwd:
+        return False, 'Please choose a different password, the username and password should not be the same!'
+
+    return True, None
+
+
+def get_logininfo():
+    uname = input("Enter a username: ")
+    if uac.check_existing_user(uname):
+        print(f"Hello {uname}! Please enter your password.")
+        
+        while True:
+            pwd = inquirer.password('Password (invisible)')            
+            pwd_hash = uac.hash_sha256(pwd)
+            if pwd_hash == uac.hash_sha256(''):
+                res = False, None
+                break
+
+            res = uac.login(uname, pwd_hash)
+            if res[0]:
+                break
+            print('Invalid password! Please try again, or press ENTER to sign up / choose a different account.')
+
+    else:
+        print("Welcome new user! Please create a password. (Minimum 8 characters long)")
+        print('Password must contain:\n\
+- At least one upper-case character\n\
+- At least one lower-case character\n\
+- At least one number\n\
+- At least one special character')
+
+        while True:
+            pwd = inquirer.password('Password (invisible)')
+            validation = validate_login(uname, pwd)
+            if not validation[0]:
+                print(validation[1])
+                continue
+                    
+            pwd_hash = uac.hash_sha256(pwd)
+            pwd1_hash = uac.hash_sha256(inquirer.password('Re-enter Password (invisible)'))
+            if pwd_hash == pwd1_hash:
+                break
+
+            print('Passwords do not match!')
+
+        uac.signup(uname, pwd_hash, hashed=True)
+        res = uac.login(uname, pwd_hash)
+
+    return res
+
+
 def print_title():
+    cols, _ = shutil.get_terminal_size()
     with open(title_file, encoding="utf-8") as tf:
+        lines = tf.readlines()
+        tf.seek(0)
+
+        max_length = max([len(l) for l in lines])
+        if cols < max_length:
+            os.system(f'mode con: cols={max_length} lines=40')
+
         print(tf.read())
 
 
@@ -33,12 +107,8 @@ def print_help():
     input()
 
 
-def __loading_animation():
-    pass
-
-
 def run_cli_solving_sequence():
-    system("cls" if os_name == "nt" else "clear")
+    os.system("cls" if os.name == "nt" else "clear")
     board = console_user_input.get_input()
     solve_state, solved_board = board_solver.solve(board)
 
@@ -56,11 +126,11 @@ def run_cli_solving_sequence():
     return
 
 
-def construct_prompt(name: str, message: str, choices: list[str]) -> list[inquirer.List]:
+def construct_prompt(name, message, choices):
     return [inquirer.List(name, message, choices)]
 
 
-def prompt_user(prompt: list[inquirer.List], name: str) -> str:
+def prompt_user(prompt, name):
     return inquirer.prompt(prompt)[name]
 
 
@@ -69,27 +139,21 @@ if __name__ == "__main__":
         print_title()
         print("\n\n\n")
 
-        # help_yn = input("Would you like to run HELP? (y/N) ").lower()
-        # if help_yn == "y":
-        #    print_help()
-
         help_prompt = construct_prompt("help_yn", "Print Help File?", ["Yes", "No"])
         help_yn = prompt_user(prompt=help_prompt, name="help_yn")
         if help_yn == "Yes":
             print_help()
 
-        # app_mode = input(
-        #     "Choose the application mode, GUI or CLI. CLI by default (GUI is not "
-        #     "avaliable right now): "
-        # ).lower()
-        # app_mode = "cli"  # TODO Change when GUI has been implemented
+        while True:
+            info = get_logininfo()
+            if info[0]:
+                break
+        print(f'\n\nHey {info[1][0]}, welcome to SudoSOLVE! Here are your user details:\n')
+        print(f'Username: {info[1][0]}\nData folder: {info[1][1]}\n\n')
+        input('Press ENTER to continue...')
+                
 
-        app_mode_prompt = construct_prompt(
-            "app_mode", "Choose application mode:", ["GUI (unavailable)", "CLI"]
-        )
-        app_mode = prompt_user(prompt=app_mode_prompt, name="app_mode")
-        app_mode = "cli"  # TODO Change after implementing GUI
-
+        app_mode = "cli" 
         if app_mode == "cli":
             while True:
                 run_cli_solving_sequence()
@@ -105,7 +169,8 @@ if __name__ == "__main__":
                 with open("assets/ascii_thanks.txt", encoding="utf-8") as thxf:
                     print(thxf.read())
                 break
-
+    
+        
     except:
         sys.exit()
-        
+    
